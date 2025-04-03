04fa424a87714681221cabc9628456d131383759
sanchojd <sanchojadon120@gmail.com>
2025-04-03 13:36:20 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight',
+  packname: 'Hunter',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "hunter Bot",
+  botOwner: 'hunter254', // Your name
+  ownerNumber: '254790759013', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
